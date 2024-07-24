49e5ceac0373ee7d92d0ee104f259c4d2b7438d8
Andrzej Rehmann <hoto@users.noreply.github.com>
2024-07-24 16:02:47 +0100

    Start calculating stuff

--- components/CopiumClock.tsx
+++ components/CopiumClock.tsx
@@ -1,23 +1,57 @@
 import { useEffect, useState } from 'react'
 
 const CopiumClock: React.FC = () => {
-  const [amount, setAmount] = useState<number>(70.00)
+  const target = new Date()
+  target.setHours(8, 0, 0, 0)
+  const [workStartTime, setWorkStartTime] = useState<Date>(target)
+  const [secondsSinceWorkStartTime, setSecondsSinceWorkStartTime] = useState<number>(0)
+  const [takeHomeMonthlySalary, setTakeHomeMonthlySalary] = useState<number>(5000)
+  const [dailyAmount, setDailyAmount] = useState<number>(0)
+  const [hourlyAmount, setHourlyAmount] = useState<number>(0)
+  const [minutlyAmount, setMinutlyAmount] = useState<number>(0)
+  const [secondlyAmount, setSecondlyAmount] = useState<number>(0)
+  const [amountMadeToday, setAmountMadeToday] = useState<number>(70.00)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setAmount(amount => amount + 0.01)
+      setAmountMadeToday(amount => amount + 0.01)
+      setSecondsSinceWorkStartTime(Math.floor((new Date().getTime() - workStartTime.getTime()) / 1000))
     }, 1000)
-
     return () => clearInterval(interval)
   }, [])
 
+  useEffect(() => {
+    setDailyAmount(takeHomeMonthlySalary / 20) // 20 working days in a month
+    setHourlyAmount(dailyAmount / 8)
+    setMinutlyAmount(hourlyAmount / 60)
+    setSecondlyAmount(minutlyAmount / 60)
+  }, [workStartTime, secondsSinceWorkStartTime])
+
   return (
-    <div className="text-9xl">
-      £{new Intl
-      .NumberFormat('en-GB', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-      .format(amount)}
+    <div>
+      <div className="text-9xl">
+        {format(amountMadeToday)}
+      </div>
+      <div className="text-xl">
+        <p>
+          Work start time: {workStartTime.toLocaleTimeString()}
+        </p>
+        <p>Take home monthly: {format(takeHomeMonthlySalary, 0, 0)} </p>
+        <p>Daily: {format(dailyAmount, 0, 0)} </p>
+        <p>Hourly: {format(hourlyAmount)} </p>
+        <p>Minutly: {format(minutlyAmount.toFixed())} </p>
+        <p>Secondly: {format(secondlyAmount.toFixed(3))} </p>
+        <br/>
+        <p>Seconds passed: {secondsSinceWorkStartTime} </p>
+      </div>
     </div>
   )
 }
 
+
+const format = (amount: number | string, min = 2, max = 2): string => {
+  return '£' + new Intl
+    .NumberFormat('en-GB', { minimumFractionDigits: min, maximumFractionDigits: max })
+    .format(Number(amount))
+}
 export default CopiumClock
